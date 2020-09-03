f07943ae3920e5c91c256ba4ea3efef0eaa49abe
KWEYUGEOFREY <38045476+KWEYUGEOFREY@users.noreply.github.com>
2020-09-03 21:40:30 +0300

    Add files via upload

---
+++ myquery.sql
@@ -0,0 +1,12 @@
+Select inst.name as "INSTITUTION NAME"
+       course.name as "COURSE NAME"
+       count(stu.student) as "NUMBER OF STUDENTS"
+       
+from   education_institution  inst
+    left join course
+    on inst.institionid = course.institution
+    left join student
+    on student.course = course.courseid
+    
+group by "NUMBER OF STUDENTS", "COURSE NAME"
+order by "NUMBER OF STUDENTS" desc;
